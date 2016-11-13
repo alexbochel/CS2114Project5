cf0131f619ffebf1c9c188e3bb300f98465abb6e
alexbochel <ajbochel@gmail.com>
2016-11-12 22:59:32 -0500

    Add files via upload

---
+++ Song.java
@@ -0,0 +1,19 @@
+package prj5;
+
+/**
+ * This class stores the information for each song. 
+ * 
+ * @author Alexander James Bochel
+ * @version 2016.11.12
+ *
+ */
+public class Song {
+
+    /**
+     * This is the cosntructor for the song class. 
+     */
+    public Song() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
