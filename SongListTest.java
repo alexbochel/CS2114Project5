30c41b05308f15b5a31f7af8dc0a618fa43ba5d8
alexbochel <ajbochel@gmail.com>
2016-11-13 14:51:55 -0500

    Add files via upload

--- SongListTest.java
+++ SongListTest.java
@@ -7,13 +7,37 @@ package prj5;
  * @version 2016.11.13
  *
  */
-public class SongListTest {
+public class SongListTest extends student.TestCase {
 
+    private SongList songList; 
+    private Song song;
+    
     /**
      * This is the setUp method for this test class.  
      */
     public void setUp() {
+        // Create the songList. 
+        songList = new SongList();
         
+        // Create songs to go into the songList. 
+        song = new Song("All You Need Is Love", 
+                "The Beatles", 1967, "pop rock");
+    }
+    
+    /**
+     * This tests the addSong method in the SongList class. 
+     */
+    public void testAddSong() {
+        assertEquals(0, songList.size());
+        songList.addSong(song);
+        assertEquals(1, songList.size());
+    }
+    
+    /**
+     * This tests the isEmpty method in the songList class. 
+     */
+    public void testIsEmpty() {
+        assertTrue(songList.isEmpty());
     }
 
 }
