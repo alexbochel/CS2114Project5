6d977c9ae9979f9ffe64a473a4fa73738d02f752
omarjas <omarjas5@vt.edu>
2016-11-13 10:26:02 -0500

    Tested likes and dislikes

--- SongTest.java
+++ SongTest.java
@@ -31,5 +31,16 @@ public class SongTest extends TestCase {
         
         String str = "All You Need Is Love: The Beatles, pop rock, 1967";
         assertEquals(str, song.toString());
+        
+        
+        assertEquals(0, song.getLikes());
+        assertEquals(0, song.getDislikes());
+        
+        song.setLikes(4);
+        song.setLikes(5);
+        assertEquals(9, song.getLikes());
+        
+        song.setDislikes(3);
+        assertEquals(3, song.getDislikes());
     }
 }
