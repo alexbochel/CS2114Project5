fede080350d51ca7a1d8762338d11e3e9dc42b7f
alexbochel <ajbochel@gmail.com>
2016-11-13 13:26:36 -0500

    Add files via upload

---
+++ SongListTest.java
@@ -0,0 +1,19 @@
+package prj5;
+
+/**
+ * This test class tests the SongList class. \
+ * 
+ * @author Alexander James Bochel
+ * @version 2016.11.13
+ *
+ */
+public class SongListTest {
+
+    /**
+     * This is the setUp method for this test class.  
+     */
+    public void setUp() {
+        
+    }
+
+}
