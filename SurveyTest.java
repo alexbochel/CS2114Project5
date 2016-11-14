a310cf4286b7d5b51867368cc19daabf160aedc2
Omar <omarjas5@vt.edu>
2016-11-14 17:32:29 -0500

    Survey Test

--- SurveyTest.java
+++ SurveyTest.java
@@ -12,30 +12,35 @@ import student.TestCase;
 public class SurveyTest extends TestCase{
 
     private Survey survey;
-    
+
     /**
-     * Set up
+     * Instantiates new survey and DoublyLinkedList<Song>
+     * for testing.
      */
     public void setUp() {
-        survey = new Survey("Computer Science", "Southeast", "sports", 
-                new DoublyLinkedList<Song>());
-        
+        DoublyLinkedList<Song> list = new DoublyLinkedList<>();
+        survey = new Survey("Computer Science", "Southeast", "sports",
+                list);
+
     }
-    
+
     /**
-     * Test
+     * Test methods in Survey class
      */
     public void testSurvey()
     {
-        
+
         assertEquals("Computer Science", survey.getMajor());
         assertEquals("Southeast", survey.getRegion());
         assertEquals("sports", survey.getHobby());
-        
+
         survey.setHobby("music");
         assertEquals("music", survey.getHobby());
-        
+
         assertEquals("Computer Science, Southeast, music", survey.toString());
+
+        assertEquals(new DoublyLinkedList<Song>().toString(),
+                survey.getList().toString());
     }
-    
+
 }
