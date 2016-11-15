9a5cacd2de55ab03cb3c9af6d7491bf824ee9c18
omarjas <omarjas5@vt.edu>
2016-11-15 12:18:49 -0500

    Update SongListTest
    Added iterator tests and tested sorting

--- SongListTest.java
+++ SongListTest.java
@@ -1,7 +1,11 @@
 package prj5;
 
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 /**
- * This test class tests the SongList class. 
+ * This test class tests the SongList class.
  *
  * @author Alexander James Bochel
  * @version 2016.11.13
@@ -20,8 +24,16 @@ public class SongListTest extends student.TestCase {
         songList = new SongList("SongListTest1.csv");
 
         // Create songs to go into the songList.
-        //song = new Song("All You Need Is Love",
-              //  "The Beatles", 1967, "pop rock");
+        song = new Song("All You Need Is Love", "The Beatles", 
+                "1967", "pop rock");
+    }
+    
+    /**
+     * Test constructor
+     */
+    public void testSongList()
+    {
+        assertEquals(5, songList.size());
     }
 
     /**
@@ -29,9 +41,18 @@ public class SongListTest extends student.TestCase {
      */
     public void testAddSong() {
 
-        assertEquals(5, songList.size());
+        
         songList.add(song);
         assertEquals(6, songList.size());
+        
+        try {
+            songList.add(null);            
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+            assertTrue(e instanceof IllegalArgumentException);
+        }
     }
 
     /**
@@ -40,4 +61,57 @@ public class SongListTest extends student.TestCase {
     public void testIsEmpty() {
         assertFalse(songList.isEmpty());
     }
+
+    /**
+     * Test sort methods
+     */
+    public void testSort() {
+        Song temp = new Song("Another One Bites the Dust", "Queen", "1980",
+                "disco");
+        
+        Song test = new Song("Anarchy in The UK", "The Sex Pistols", 
+                "1976", "punk");
+        
+        songList.sortGenre();
+        
+        assertEquals(temp.toString(), songList.get(1).toString());
+        assertEquals(song.toString(), songList.get(3).toString());
+        assertEquals(test.toString(), songList.get(4).toString());
+        
+        songList.sortTitle();
+        
+        assertEquals(song.toString(), songList.get(1).toString());
+        assertEquals(test.toString(), songList.get(3).toString());
+        assertEquals(temp.toString(), songList.get(4).toString());
+    }
+    
+    /**
+     * Test iterator methods
+     */
+    public void testIterator()
+    {
+        songList.sortTitle();
+        System.out.print(songList);
+        Iterator<Song> iter = songList.iterator();
+        iter.next();
+        
+        // Check next and hasNext
+        assertEquals(iter.next().toString(), song.toString());
+        assertTrue(iter.hasNext());
+        iter.next();
+        iter.next();
+        iter.next();
+        assertFalse(iter.hasNext());
+        
+        // Check when text is called at the end of the list
+        try {
+            iter.next();
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+            assertTrue(e instanceof NoSuchElementException);
+        }
+    }
+
 }
