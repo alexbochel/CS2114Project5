bbde5f1a813a00213aac02fe52265a8d45021346
omarjas <omarjas5@vt.edu>
2016-11-27 17:19:11 -0500

    Update SongListTest
    Added tests for heard, notHeard, liked, and notLiked for major and region.

--- SongListTest.java
+++ SongListTest.java
@@ -1,114 +1,233 @@
 package prj5;
 
-import java.io.FileNotFoundException;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
+import student.TestCase;
 /**
- * This test class tests the SongList class.
  *
- * @author Alexander James Bochel
- * @version 2016.11.13
- *
+ * @author Omar, Alex, Purnima
+ * @version 2016.11.14
  */
-public class SongListTest extends student.TestCase {
+public class SongTest extends TestCase {
 
-    private SongList songList;
     private Song song;
+    private Song song2;
 
     /**
-     * This is the setUp method for this test class.
+     * This is the setup method for the test class. 
      */
-    public void setUp() {
-        // Create the songList.
-        try {
-            songList = new SongList("SongListTest1.csv");
-        } 
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        // Create songs to go into the songList.
-        song = new Song("All You Need Is Love", "The Beatles", 
-                "1967", "pop rock");
-    }
-    
-    /**
-     * This tests the addSong method in the SongList class.
-     */
-    public void testAddSong() {
-
-        
-        songList.add(song);
-        assertEquals(6, songList.size());
-        
-        try {
-            songList.add(null);            
-        }
-        catch (Exception e)
-        {
-            assertNotNull(e);
-            assertTrue(e instanceof IllegalArgumentException);
-        }
-    }
-
-    /**
-     * This tests the isEmpty method in the songList class.
-     */
-    public void testIsEmpty() {
-        assertFalse(songList.isEmpty());
-    }
-
-    /**
-     * This tests the sort methods for the SongList class. 
-     */
-    public void testSort() {
-        Song temp = new Song("Another One Bites the Dust", "Queen", "1980",
-                "disco");
-        
-        Song test = new Song("Anarchy in The UK", "The Sex Pistols", 
-                "1976", "punk");
-        
-        songList.sortGenre();
-        
-        assertEquals(temp.toString(), songList.get(1).toString());
-        assertEquals(song.toString(), songList.get(3).toString());
-        assertEquals(test.toString(), songList.get(4).toString());
-        
-        songList.sortTitle();
-        
-        assertEquals(song.toString(), songList.get(1).toString());
-        assertEquals(test.toString(), songList.get(3).toString());
-        assertEquals(temp.toString(), songList.get(4).toString());
-    }
-    
-    /**
-     * This tests the iterator methods in the nested class. 
-     */
-    public void testIterator()
+    public void setUp()
     {
-        songList.sortTitle();
-        System.out.print(songList);
-        Iterator<Song> iter = songList.iterator();
-        iter.next();
+        song = new Song("All You Need Is Love",
+                "The Beatles", "1967", "pop rock");
+        song2 = new Song("Fat Lip",
+                "Sum 41", "1967", "alternative");
+    }
+
+    /**
+     * This method tests the set likes method for each hobby.
+     */
+    public void testGetMethods() {
+        // Liked
+        assertEquals(0, song.getArtTotalLiked());
+        assertEquals(0, song.getArtLiked());
+
+        assertEquals(0, song.getReadingTotalLiked());
+        assertEquals(0, song.getReadingLiked());
+
+        assertEquals(0, song.getMusicTotalLiked());
+        assertEquals(0, song.getMusicLiked());
+
+        assertEquals(0, song.getSportsTotalLiked());
+        assertEquals(0, song.getSportsLiked());
+
+        // Heard
+        assertEquals(0, song.getArtTotalHeard());
+        assertEquals(0, song.getArtHeard());
+
+        assertEquals(0, song.getReadingTotalHeard());
+        assertEquals(0, song.getReadingHeard());
+
+        assertEquals(0, song.getMusicTotalHeard());
+        assertEquals(0, song.getMusicHeard());
+
+        assertEquals(0, song.getSportsTotalHeard());
+        assertEquals(0, song.getSportsHeard());
+
+        // Song Information
+        assertEquals("All You Need Is Love", song.getTitle());
+        assertEquals("The Beatles", song.getArtist());
+        assertEquals("1967", song.getYear());
+        assertEquals("pop rock", song.getGenre());
+
+        // toString
+        String str = "song title all you need is love\nsong artist "
+                + "the beatles\nsong genre pop rock\nsong year 1967";
+        assertEquals(str, song.toString());
+    }
+
+    /**
+     * This method tests the compareToTitle methods for the song class. 
+     */
+    public void testCompareToTitle() {
+        assertEquals(-1, song.compareToTitle(song2));
+        assertEquals(1, song2.compareToTitle(song));
+        assertEquals(0, song.compareToTitle(song));
+    }
+
+    /**
+     * This method tests the compareToGenre method. 
+     */
+    public void testCompareToGenre() {
+        assertEquals(1, song.compareToGenre(song2));
+        assertEquals(-1, song2.compareToGenre(song));
+        assertEquals(0, song.compareToGenre(song));
+    }    
+
+    /**
+     * Test heard and not heard methods in Song class. 
+     */
+    public void testHeard()
+    {
+        // Art
+        song.notHeard("art", "Computer Science", "Northeast");
+        assertEquals(1, song.getArtTotalHeard());
+        assertEquals(1, song.getCsTotalHeard());
+        assertEquals(1, song.getNortheastTotalHeard());
+        song.heard("art", "Computer Science", "Northeast");
+        assertEquals(2, song.getArtTotalHeard());
+        assertEquals(1, song.getArtHeard());
+        assertEquals(2, song.getCsTotalHeard());
+        assertEquals(1, song.getCsHeard());
+        assertEquals(2, song.getNortheastTotalHeard());
+        assertEquals(1, song.getNortheastHeard());
         
-        // Check next and hasNext
-        assertEquals(iter.next().toString(), song.toString());
-        assertTrue(iter.hasNext());
-        iter.next();
-        iter.next();
-        iter.next();
-        assertFalse(iter.hasNext());
+        song.notLiked("art", "Computer Science", "Northeast");
+        assertEquals(1, song.getArtTotalLiked());
+        assertEquals(1, song.getCsTotalLiked());
+        assertEquals(1, song.getNortheastTotalLiked());
+        song.liked("art", "Computer Science", "Northeast");
+        assertEquals(2, song.getArtTotalLiked());
+        assertEquals(1, song.getArtLiked());
+        assertEquals(2, song.getCsTotalLiked());
+        assertEquals(1, song.getCsLiked());
+        assertEquals(2, song.getNortheastTotalLiked());
+        assertEquals(1, song.getNortheastLiked());
+
+        // Reading
+        song.notHeard("reading", "Math or CMDA", "Southeast");
+        assertEquals(1, song.getMathTotalHeard());
+        assertEquals(1, song.getReadingTotalHeard());
+        assertEquals(1, song.getSoutheastTotalHeard());
+        song.heard("reading", "Math or CMDA", "Southeast");
+        assertEquals(2, song.getReadingTotalHeard());
+        assertEquals(1, song.getReadingHeard());
+        assertEquals(2, song.getMathTotalHeard());
+        assertEquals(1, song.getMathHeard());
+        assertEquals(2, song.getSoutheastTotalHeard());
+        assertEquals(1, song.getSoutheastHeard());
+
+        song.notLiked("reading", "Math or CMDA", "Southeast");
+        assertEquals(1, song.getMathTotalLiked());
+        assertEquals(1, song.getReadingTotalLiked());
+        assertEquals(1, song.getSoutheastTotalLiked());
+        song.liked("reading", "Math or CMDA", "Southeast");
+        assertEquals(2, song.getReadingTotalLiked());
+        assertEquals(1, song.getReadingLiked());
+        assertEquals(2, song.getMathTotalLiked());
+        assertEquals(1, song.getMathLiked());
+        assertEquals(2, song.getSoutheastTotalLiked());
+        assertEquals(1, song.getSoutheastLiked());
+
+        // Sports
+        song.notHeard("sports", "Other Engineering",
+                "United States (other than Southeast or Northwest)");
+        assertEquals(1, song.getSportsTotalHeard());
+        assertEquals(1, song.getEngineeringTotalHeard());
+        assertEquals(1, song.getUnitedstatesTotalHeard());
+        song.heard("sports", "Other Engineering",
+                "United States (other than Southeast or Northwest)");
+        assertEquals(2, song.getSportsTotalHeard());
+        assertEquals(1, song.getSportsHeard());
+        assertEquals(2, song.getEngineeringTotalHeard());
+        assertEquals(1, song.getEngineeringHeard());
+        assertEquals(2, song.getUnitedstatesTotalHeard());
+        assertEquals(1, song.getUnitedstatesHeard());
+
+        song.notLiked("sports", "Other Engineering",
+                "United States (other than Southeast or Northwest)");
+        assertEquals(1, song.getSportsTotalLiked());
+        assertEquals(1, song.getEngineeringTotalLiked());
+        assertEquals(1, song.getUnitedstatesTotalLiked());
+        song.liked("sports", "Other Engineering",
+                "United States (other than Southeast or Northwest)");
+        assertEquals(2, song.getSportsTotalLiked());
+        assertEquals(1, song.getSportsLiked());
+        assertEquals(2, song.getEngineeringTotalLiked());
+        assertEquals(1, song.getEngineeringLiked());
+        assertEquals(2, song.getUnitedstatesTotalLiked());
+        assertEquals(1, song.getUnitedstatesLiked());
+
         
-        // Check when text is called at the end of the list
+        // Music
+        song.notHeard("music", "Other", "Outside of United States");
+        assertEquals(1, song.getMusicTotalHeard());
+        assertEquals(1, song.getOtherTotalHeard());
+        assertEquals(1, song.getOutsideTotalHeard());
+        song.heard("music", "Other", "Outside of United States");
+        assertEquals(2, song.getMusicTotalHeard());
+        assertEquals(1, song.getMusicHeard());
+        assertEquals(2, song.getOtherTotalHeard());
+        assertEquals(1, song.getOtherHeard());
+        assertEquals(2, song.getOutsideTotalHeard());
+        assertEquals(1, song.getOutsideHeard());
+
+        song.notLiked("music", "Other", "Outside of United States");
+        assertEquals(1, song.getMusicTotalLiked());
+        assertEquals(1, song.getOtherTotalLiked());
+        assertEquals(1, song.getOutsideTotalLiked());
+        song.liked("music", "Other", "Outside of United States");
+        assertEquals(2, song.getMusicTotalLiked());
+        assertEquals(1, song.getMusicLiked());
+        assertEquals(2, song.getOtherTotalLiked());
+        assertEquals(1, song.getOtherLiked());
+        assertEquals(2, song.getOutsideTotalLiked());
+        assertEquals(1, song.getOutsideLiked());
+
+        // Default
         try {
-            iter.next();
+            song.heard("fishing", "swimming", "running");
         }
         catch (Exception e)
         {
             assertNotNull(e);
-            assertTrue(e instanceof NoSuchElementException);
+            assertTrue(e instanceof IllegalStateException);
         }
-    }
 
+        try {
+            song.notHeard("fishing", "swimming", "running");
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+            assertTrue(e instanceof IllegalStateException);
+        }
+
+        try {
+            song.liked("fishing", "swimming", "running");
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+            assertTrue(e instanceof IllegalStateException);
+        }
+
+        try {
+            song.notLiked("fishing", "swimming", "running");
+        }
+        catch (Exception e)
+        {
+            assertNotNull(e);
+            assertTrue(e instanceof IllegalStateException);
+        }
+        
+    }
 }
