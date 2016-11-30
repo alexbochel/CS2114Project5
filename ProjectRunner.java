73e0237f5159f1e5d47714efbaee85faf11089ad
omarjas <omarjas5@vt.edu>
2016-11-30 13:02:11 -0500

    Hard coded files for fall 2016

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -2,9 +2,10 @@ package prj5;
 
 public class ProjectRunner {
 
-    public static void main(String[] args)
-    {
-        Input songs = new Input("SongList.csv", "MusicSurveyData.csv");
+    public static void main(String[] args) {
+        Input songs = new Input("MusicSurveyData2016F.csv",
+                "SongList2016F.csv");
+
         GUIWindow window = new GUIWindow(songs);
     }
 }
